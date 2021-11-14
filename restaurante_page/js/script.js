414eadb33dd22027533937b1470ba6a7c115a012
JohnnyDev2001 <johnny.devcode@gmail.com>
2021-11-14 19:46:38 -0300

    produtos

--- restaurante_page/js/script.js
+++ restaurante_page/js/script.js
@@ -1,7 +1,37 @@
 const menu = document.querySelector('#menu-bars');
 const navbar = document.querySelector('.navbar');
 
+const search_icon = document.querySelector('#search-icon');
+const closeit = document.querySelector('#close');
+
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+search_icon.onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+closeit.onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2700,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
